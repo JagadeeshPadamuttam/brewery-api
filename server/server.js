0eee9c00c447f6c72efce82ceb814696c4f68c1b
JagadeeshPadamuttam <88880051+JagadeeshPadamuttam@users.noreply.github.com>
2023-11-13 19:41:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/brewery_reviews", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/brewery_reviews");
 
 const db = mongoose.connection;
 
